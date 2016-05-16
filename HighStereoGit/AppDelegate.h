417b7d456bedb903a47f7232efaaf1a6f4faeb82
insubae <insubae@hotmail.com>
2016-05-16 09:19:46 +0900

    Initial Commit

---
+++ HighStereoGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HighStereoGit
+//
+//  Created by Insu Bae on 2016. 5. 16..
+//  Copyright © 2016년 Insu Bae. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HighStereoGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HighStereoGit
+//
+//  Created by Insu Bae on 2016. 5. 16..
+//  Copyright © 2016년 Insu Bae. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
